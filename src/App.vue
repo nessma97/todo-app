d7763a5a1bed95e25f735738115935a2673f3d7c
nessma97 <nessmaelsheikh97@gmail.com>
2022-06-10 04:06:37 +0200

    created the todo list component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="shadow-xl  bg-teal-400">
+    <h1 class=" p-1">Add ToDos below</h1>
+    <add-task @toDo__added="addTodo($event)" @allRemoved="removeAll($event)"/>
+    <task-list :todos="todos" @deleted="deleteTodo($event)" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddTask from './components/AddTask.vue'
+import TaskList from './components/TaskList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AddTask,
+    TaskList
+  },
+  data () {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    addTodo ({ task }) {
+      this.todos.push({ task })
+      // console.log(task)
+      localStorage.setItem('todos', JSON.stringify(this.todos))
+    },
+    deleteTodo ({ i }) {
+      this.todos.splice(i, 1)
+      localStorage.getItem('todos', JSON.parse(this.todos))
+    },
+    removeAll () {
+      this.todos = []
+      // this.todos.length = 0
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 40%;
+  max-height: 50%;
+  padding: 10px;
+  margin: 100px auto;
+  background-color: var(--main-color);
+  border-radius: 10px;
+}
+
+#app h1 {
+  font-style: italic;
+  font-size: larger;
+  font-weight: 400;
+  margin-left: 15px;
+  color: white;
 }
 </style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="task-list">
+    <ul class="todos" v-for='(todo, i) in todos' :key="i">
+      <li class="todos__items" >
+        <span>{{ todo.task }}</span>
+        <button @click="deleteTodo(i)" class="delete__btn">
+          <img src="../assets/images/delete.png" alt="delete icon" class="delete-icon">
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskList',
+  props: {
+    todos: Array
+  },
+  methods: {
+    deleteTodo (i) {
+      this.$emit('deleted', { i })
+    }
+  }
+}
+</script>
+
+<style>
+.todos {
+  width: 100%;
+  max-width: 80%;
+  margin-block: 10px;
+  text-align: center;
+}
+
+.delete__btn {
+    cursor: pointer;
+    transition: border .5s ease-in-out;
+    width: 7%;
+}
+
+.todos__items {
+  display: flex;
+  justify-content:space-between;
+  align-items: center;
+  margin: 10px 5px 15px 15px;
+  color:var(--dark-moov);
+  transition: border .3s ease-in-out;
+  border-bottom: 2px solid var(--light-moov);
+  padding: .5rem;
+  }
+
+  .todos__items:hover {
+  border-color: var(--secondary-color);
+  }
+
+</style>
